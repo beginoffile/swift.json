2cb3f307f1bac2e0535f61eb4960f9d0860b7a6f
cesar <cursos.mapatupisaro@gamail.com>
2024-02-21 17:53:59 -0600

    Initial Commit

---
+++ DemoJson/DemoJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoJsonApp.swift
+//  DemoJson
+//
+//  Created by Aguid Ramirez Sanchez on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
